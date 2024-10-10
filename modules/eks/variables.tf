9a9c4063549ec18678e13eac8ba381579573e212
hidoldori <30795255+hidoldori@users.noreply.github.com>
2024-10-10 13:35:41 +0900

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,1 +1,10 @@
 #-- variables.tf
+
+
+variable "subnett_ids" {
+  description = "A list of CIDR blocks to use for the public subnets."
+  type        = list(string)
+
+  #-- multi az 이 필요함.
+  //default     = ["subnet-0fedd10ecf55bbcc2","subnet-09d3d345818797715"]
+}
