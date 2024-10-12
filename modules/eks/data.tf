2bf554c1663e97f266a700831f075486e1110d37
hidoldori <30795255+hidoldori@users.noreply.github.com>
2024-10-12 21:41:10 +0900

    Update data.tf

--- modules/eks/data.tf
+++ modules/eks/data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
-    bucket = "gosu-terraform"
+    bucket = "gosu-terraform-app"
     key    = "teraform.state"
     region = "ap-northeast-2"
   }
