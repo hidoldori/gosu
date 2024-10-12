7172e92648ce68eb192450cd08e8712bb49da1a6
hidoldori <30795255+hidoldori@users.noreply.github.com>
2024-10-12 21:40:19 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     region  = "ap-northeast-2"
     key     = "teraform.state" #state file 명
-    bucket  = "gosu-terraform" #기 생성한 bucket 명 
+    bucket  = "gosu-terraform-app" #기 생성한 bucket 명 
   }
 }
